b87f71bb8a82a549fd17b28a96c29832fc653191
elena_kau <lelickau@yandex.ru>
2021-05-20 16:34:33 +0200

    1.2

---
+++ js/smothScroll.js
@@ -0,0 +1,14 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const smothScrollElems = document.querySelectorAll('a[href^="#"]:not(a[href="#"])');
+
+    smothScrollElems.forEach(link => {
+        link.addEventListener('click', (e) => {
+            e.preventDefault();
+            const id = link.getAttribute('href').substring(1);
+
+            document.getElementById(id).scrollIntoView({
+                behavior: 'smooth'
+            });
+        })
+    })
+});
