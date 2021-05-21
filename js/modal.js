a19ba6cfbabcdb93214f39c85dd6ee01eea0b5cb
elena_kau <lelickau@yandex.ru>
2021-05-21 16:41:58 +0200

    1.3

--- js/modal.js
+++ js/modal.js
@@ -2,24 +2,51 @@ document.addEventListener('DOMContentLoaded', () => {
     
 });
 
-const moreElems = document.querySelectorAll('.more');
+// const moreElems = document.querySelectorAll('.more');
+// const modalElem = document.querySelector('.modal');
+
+
+//     const openModal = () => {
+//         modalElem.classList.remove('hidden');
+//         //
+//         disableScroll();
+//     }
+//     const closeModal = () => {
+//         modalElem.classList.add('hidden');
+//         //
+//         enableScroll();
+//     }
+
+//     moreElems.forEach(moreElem => {
+//         moreElem.addEventListener('click', openModal);
+//     });
+    
+//     modalElem.addEventListener('click', (e) => {
+//         const target = e.target;
+//         if(target.classList.contains('overlay') || target.classList.contains('modal__close')) closeModal();
+//     })
+
+
+    // вариант с помощью делигирования
+
 const modalElem = document.querySelector('.modal');
+const designBlock = document.querySelector('.design-block');
 
     const openModal = () => {
         modalElem.classList.remove('hidden');
-        //
+        //!import
         disableScroll();
     }
     const closeModal = () => {
         modalElem.classList.add('hidden');
-        //
+        //!import
         enableScroll();
     }
-
-    moreElems.forEach(moreElem => {
-        moreElem.addEventListener('click', openModal);
-    });
     
+    designBlock.addEventListener('click', (e) => {
+        const target = e.target;
+        if(target.matches('.more')) openModal();
+    })
     modalElem.addEventListener('click', (e) => {
         const target = e.target;
         if(target.classList.contains('overlay') || target.classList.contains('modal__close')) closeModal();
